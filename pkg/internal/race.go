d48d7c8f21afb8933885b584a05ba5aa52e0d85f
zizon <zzdtsv@gmail.com>
2020-08-25 19:50:59 +0800

    simplify race state control

--- pkg/internal/race.go
+++ pkg/internal/race.go
@@ -23,49 +23,67 @@ type raceBundle struct {
 
 func receConnect(bundle raceBundle) io.ReadWriter {
 	ready := make(chan io.ReadWriter)
+	defer close(ready)
 
-	raceCtx := bundle.ctx.Derive(nil)
-	raceCtx.Cleanup(func() error {
-		close(ready)
-		return nil
-	})
+	connectStates := make([]CanclableContext, len(bundle.connectors))
 	// go race connect
-	for _, connector := range bundle.connectors {
+	for i, connector := range bundle.connectors {
 		// raceCtx are used for syncronize,
 		// do not attatch to it, or connection
 		// may be drop immediatly
 		connectCtx := bundle.ctx.Derive(nil)
+		connectStates[i] = connectCtx
 
 		go connector(connectBundle{
 			connectCtx,
 			func(rw io.ReadWriter) {
-				defer raceCtx.Cancle()
-
 				// wait for ready or die
 				select {
 				case ready <- rw:
 					// first ready connection wins
-					LogInfo("win race connect -> %s:%d %v", bundle.addr, bundle.port, reflect.TypeOf(rw))
 					return
-				case <-raceCtx.Done():
+				case <-connectCtx.Done():
 					// or someone had already call pushReady,
 					// cancel so as to free up resources
-					connectCtx.Cancle()
 					return
 				}
-
 			},
 			bundle.addr,
 			bundle.port,
 		})
 	}
 
-	select {
-	case rw := <-ready:
-		// pull first ready
-		return rw
-	case <-bundle.ctx.Done():
-		// or canceled
-		return nil
+	// 1. some race success
+	// 2. all race faile
+	// 3. race canceld
+	for i, state := range connectStates {
+		select {
+		case rw := <-ready:
+			LogInfo("win race connect -> %s:%d %v", bundle.addr, bundle.port, reflect.TypeOf(rw))
+			// go cacenl the others
+			// use i is ok,since no more modification?
+			go func() {
+				for j, ctx := range connectStates {
+					if i != j {
+						ctx.Cancle()
+					}
+				}
+			}()
+
+			return rw
+		case <-state.Done():
+			// this connect fail,
+			// wait for next
+			LogInfo("some race was cancel")
+			continue
+		case <-bundle.ctx.Done():
+			// cancel as a whole
+			LogInfo("race wase cancel as a whole")
+			return nil
+		}
 	}
+
+	LogWarn("all race fail")
+	// all fail
+	return nil
 }
